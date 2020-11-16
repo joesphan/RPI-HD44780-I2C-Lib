a5e8fa96bdd94500fc7e786f9606d27732c5a972
Joesphan Lu <38387138+joesphan@users.noreply.github.com>
2020-11-16 00:00:41 -0800

    Update I2C_LCD_driver.py

--- I2C_LCD_driver.py
+++ I2C_LCD_driver.py
@@ -4,6 +4,7 @@
 
 """
 Compiled, mashed and generally mutilated 2014-2015 by Denis Pleic
+Furthur mutilated 2020 by Joesphan Lu
 Made available under GNU GENERAL PUBLIC LICENSE
 
 # Modified Python I2C library for Raspberry Pi
